ab6962b564a1c84fa71453aac65abd1d3c43fe14
LuisSid55 <luiscitty4@gmail.com>
2021-09-25 02:43:28 -0300

    Milhas para K

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MilhasParaKm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double mi, km = 1.609;
+            Console.WriteLine("\n---Milhas para Km---");
+            
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.Write("\nDiga em milhas: ");
+            mi = Convert.ToDouble(Console.ReadLine());
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write($"\nQue eu digo:    {mi * km} Km\n");
+            Console.ResetColor();
+        }
+    }
+}
